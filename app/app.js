cbd582ae76d55863fc177d349302fc688bff3d41
Avula Chandra Shekar <avulachandrashekar12@gmail.com>
2017-09-10 02:03:42 +0800

    For heroku

--- app/app.js
+++ app/app.js
@@ -5,11 +5,9 @@ app.use(express.static(__dirname + "/../src/"));
 app.use(express.static(__dirname + "../src/assets"));
 
 app.set('port', (process.env.PORT || 8000));
-
 app.get('/', function(req, res){
         res.send(__dirname + '/../src/index.html');
 });
-
 app.listen(app.get('port'), function(){
     console.log("listening on " + app.get('port'));
 });
